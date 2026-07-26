fb9cde12c42137bbf6b935ca04b3562d286689da
KamouloxPelvis <kamoulox_pelvis@proton.me>
2026-07-26 11:41:57 +0200

    chore: initialize K-Guard AI project

---
+++ src/main/java/org/devopsnotes/kguard/ai/KGuardAiApplication.java
@@ -0,0 +1,13 @@
+package org.devopsnotes.kguard.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KGuardAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KGuardAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/devopsnotes/kguard/ai/KGuardAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devopsnotes.kguard.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KGuardAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
